c6947164f08190627ac5bf79f99da9ffb08b0c98
Krepi <102362873+Wh12300@users.noreply.github.com>
2023-10-12 19:32:08 +0400

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,14 +1,11 @@
 import discord
 
-# Initialize the bot
 bot = discord.Client()
 
-# Define an event for when the bot is ready
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
-# Define an event for when the bot receives a message
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -17,5 +14,4 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
-# Run the bot with your token
 bot.run('YOUR_BOT_TOKEN')
